43992221b646704134439a0181efc51e7d7eb65a
shaojiasong <lesonky@163.com>
2022-05-20 10:14:42 +0800

    feat: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,10 +22,6 @@ module.exports = {
       jsx: true,
     },
   },
-  settings: {
-    // use default config:
-    'import/resolver': 'vite',
-  },
   // eslint-plugin-vue @typescript-eslint/eslint-plugin eslint-plugin-prettier的缩写
   plugins: ['vue', '@typescript-eslint', 'prettier'],
   rules: {
